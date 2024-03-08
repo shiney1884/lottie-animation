5afe1e0a3a110cde1cf2aa5f1edaa311bc2e8cdf
Scott <scott0406@outlook.com>
2024-03-08 13:59:30 +0000

    fixing headings

--- src/InfoElement.js
+++ src/InfoElement.js
@@ -4,7 +4,7 @@ function InfoElement({icon, heading, text, color}) {
     return(
         <div className="mt-6 lg:mt-0 w-72 flex flex-col justify-between items-start h-36">
             <FontAwesomeIcon icon={icon} size="2xl" color={color}/>
-            <h3 className="font-bold text-gray-800">{heading}</h3>
+            <h2 className="font-bold text-gray-800">{heading}</h2>
             <p className="text-gray-500">{text}</p>
         </div>
     )
