3c571922f35f0ad906215ff18d8fe588a2f4004d
Scott <scott0406@outlook.com>
2024-03-08 13:47:30 +0000

    change to darker green

--- src/Button.js
+++ src/Button.js
@@ -1,6 +1,6 @@
 function Button() {
     return (
-        <a className="rounded-lg bg-green-500 text-white p-3 w-48 flex justify-center font-medium" href="#">Try It For Free</a>
+        <a className="rounded-lg bg-green-700 text-white p-3 w-48 flex justify-center font-medium" href="#">Try It For Free</a>
     )
 }
 
